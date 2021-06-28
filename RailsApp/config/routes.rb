d30f8f9a91df5fc30d25dedf9ba1cf240564a78f
yonePC <ys.mailaddress0929@gmail.com>
2021-06-28 12:20:18 +0000

    rails study3-2

--- RailsApp/config/routes.rb
+++ RailsApp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "people" , to: 'people#index'
   get 'msgboard/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "msgboard" , to: "msgboard#index"
